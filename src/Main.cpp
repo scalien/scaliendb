98b8d149b10f6aeebb1a9e29f221d02e0e8a9e51
Attila Gazso <agazso@scalien.com>
2010-07-31 16:11:35 +0200

    Created data config.

--- src/Main.cpp
+++ src/Main.cpp
@@ -3,6 +3,7 @@
 #include "Framework/Replication/ReplicationManager.h"
 #include "Application/Controller/ControlConfigContext.h"
 #include "Application/Controller/Controller.h"
+#include "Application/DataNode/DataNode.h"
 #include "Application/HTTP/HttpServer.h"
 
 int main(int argc, char** argv)
@@ -11,6 +12,7 @@ int main(int argc, char** argv)
 	Database					db;
 	ControlConfigContext		ctx;
 	Controller					controller;
+	DataNode					dataNode;
 	SingleQuorum				quorum;
 	QuorumDatabase				qdb;
 	QuorumTransport				qtransport;
@@ -18,8 +20,15 @@ int main(int argc, char** argv)
 	HttpServer					httpServer;
 	Endpoint					endpoint;
 	uint64_t					nodeID;
+	bool						isController;
+	const char*					role;
 	
 	configFile.Init(argv[1]);
+	role = configFile.GetValue("role", "data");
+	if (strcmp(role, "data") == 0)
+		isController = false;
+	else
+		isController = true;
 
 	Log_SetTarget(LOG_TARGET_STDOUT);
 	Log_SetTrace(true);
@@ -29,13 +38,16 @@ int main(int argc, char** argv)
 
 	dbConfig.dir = configFile.GetValue("database.dir", DATABASE_CONFIG_DIR);
 	db.Init(dbConfig);
+
+	if (isController)
+	{
+		nodeID = configFile.GetIntValue("nodeID", 0);
+		RMAN->SetNodeID(nodeID);
+	}
 	
-	nodeID = configFile.GetIntValue("nodeID", 0);
-	RMAN->SetNodeID(nodeID);
-	
-	const char* s = configFile.GetListValue("controllers", nodeID, NULL);
+	const char* s = configFile.GetListValue("controllers", RMAN->GetNodeID(), NULL);
 	endpoint.Set(s);
-	RMAN->GetTransport()->Init(nodeID, endpoint);
+	RMAN->GetTransport()->Init(RMAN->GetNodeID(), endpoint);
 	
 	unsigned numNodes = configFile.GetListNum("controllers");
 	for (unsigned i = 0; i < numNodes; i++)
@@ -47,28 +59,37 @@ int main(int argc, char** argv)
 		// TODO: i will not be a nodeID once we start using UIDs
 		RMAN->GetTransport()->AddEndpoint(i, endpoint);
 	}
-	
-	qdb.Init(db.GetTable("keyspace"));
-	prefix.Write("0");
-	qtransport.SetPriority(true);
-	qtransport.SetPrefix(prefix);
-	qtransport.SetQuorum(&quorum);
-	
-	ctx.SetContextID(0);
-	ctx.SetController(&controller);
-	ctx.SetQuorum(quorum);
-	ctx.SetDatabase(qdb);
-	ctx.SetTransport(qtransport);
-	ctx.Start();
 
-	controller.Init(db.GetTable("keyspace"));
-	controller.SetConfigContext(&ctx);
-	RMAN->GetTransport()->SetController(&controller); // TODO: hack
+	if (isController)
+	{
+		qdb.Init(db.GetTable("keyspace"));
+		prefix.Write("0");
+		qtransport.SetPriority(true);
+		qtransport.SetPrefix(prefix);
+		qtransport.SetQuorum(&quorum);
+		
+		ctx.SetContextID(0);
+		ctx.SetController(&controller);
+		ctx.SetQuorum(quorum);
+		ctx.SetDatabase(qdb);
+		ctx.SetTransport(qtransport);
+		ctx.Start();
+
+		controller.Init(db.GetTable("keyspace"));
+		controller.SetConfigContext(&ctx);
+		RMAN->GetTransport()->SetController(&controller); // TODO: hack
+		RMAN->AddContext(&ctx);
+
+		httpServer.Init(configFile.GetIntValue("http.port", 8080));
+		httpServer.RegisterHandler(&controller);
+	}
+	else
+	{
+		dataNode.Init(db.GetTable("keyspace"));
+		httpServer.Init(configFile.GetIntValue("http.port", 8080));
+		httpServer.RegisterHandler(&dataNode);
+	}
 	
-	httpServer.Init(configFile.GetIntValue("http.port", 8080));
-	httpServer.RegisterHandler(&controller);
-	
-	RMAN->AddContext(&ctx);
 	
 	EventLoop::Init();
 	EventLoop::Run();
