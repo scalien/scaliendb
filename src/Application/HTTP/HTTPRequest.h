f8cac54634043619ab84f317b720cb1d0d22f5d2
Scalien <info@scalien.com>
2010-08-16 16:11:42 +0200

    Renamed Http* to HTTP*.

---
+++ src/Application/HTTP/HTTPRequest.h
@@ -0,0 +1,36 @@
+#ifndef HTTP_REQUEST_H
+#define HTTP_REQUEST_H
+
+#include "IMF.h"
+
+/*
+===============================================================================
+
+ HTTPRequest
+
+===============================================================================
+*/
+
+class HTTPRequest
+{
+public:
+	typedef IMFHeader::RequestLine RequestLine;
+	enum State
+	{
+		REQUEST_LINE,
+		HEADER,
+		CONTENT
+	};
+
+	IMFHeader		header;
+	RequestLine		line;
+	State			state;
+	int				pos;
+	int				contentLength;
+	
+	void			Init();
+	void			Free();
+	int				Parse(char *buf, int len);
+};
+
+#endif
