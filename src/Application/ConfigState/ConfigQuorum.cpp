4e8d81ab2b9b97e73c0d5bea8aaaae58975dc4c6
Marton Trencseni <mtrencseni@gmail.com>
2010-10-19 10:58:05 +0200

    Shard server activation related bugfix.

--- src/Application/ConfigState/ConfigQuorum.cpp
+++ src/Application/ConfigState/ConfigQuorum.cpp
@@ -26,6 +26,14 @@ ConfigQuorum& ConfigQuorum::operator=(const ConfigQuorum& other)
     uint64_t*   sit;
     
     quorumID = other.quorumID;
+    configID = other.configID;
+    isActivatingNode = other.isActivatingNode;
+    isWatchingPaxosID = other.isWatchingPaxosID;
+    isReplicatingActivation = other.isReplicatingActivation;
+    activatingNodeID = other.activatingNodeID;
+    activationPaxosID = other.activationPaxosID;
+    activationExpireTime = other.activationExpireTime;
+    
     activeNodes = other.activeNodes;
     inactiveNodes = other.inactiveNodes;
     
