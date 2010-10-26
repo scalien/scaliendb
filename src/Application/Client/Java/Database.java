f1a9b5a59463a32a06756eb5ad124bd3193c9f83
Attila Gazso <agazso@scalien.com>
2010-10-26 20:29:42 +0200

    Fixed Java Database class name initialization.

--- src/Application/Client/Java/Database.java
+++ src/Application/Client/Java/Database.java
@@ -8,12 +8,13 @@ public class Database
     private long databaseID;
     private String name;
     
-    public Database(Client client_, String name) throws SDBPException {
+    public Database(Client client_, String name_) throws SDBPException {
         client = client_;
         BigInteger bi = scaliendb_client.SDBP_GetDatabaseID(client.getPtr(), name);
         databaseID = bi.longValue();
         if (databaseID == 0)
-            throw new SDBPException(Status.toString(Status.SDBP_BADSCHEMA));            
+            throw new SDBPException(Status.toString(Status.SDBP_BADSCHEMA));
+        name = name_;
     }
     
     public long getDatabaseID() {
