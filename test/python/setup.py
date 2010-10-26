b5b0fd6fb44da06b6219c9bfc595894d946b7566
Marton Trencseni <mtrencseni@gmail.com>
2010-10-26 19:27:43 +0200

    Fixed up Python test program.

--- test/python/setup.py
+++ test/python/setup.py
@@ -1,14 +1,20 @@
 import scaliendb
 
 client = scaliendb.Client(["127.0.0.1:7080"])
-client._set_trace(True)
 
-print(client.create_quorum(["100"]))
+quorum_id = client.create_quorum(["100"])
+print("Creating quorum(%s)..." % quorum_id)
 
-print(client.create_database("message_board"))
-print(client.create_table(1, 1, "messages"))
+database_id = client.create_database("message_board")
+print("Creating database message_board(%s)..." % database_id)
+
+table_id = client.create_table(int(database_id), int(quorum_id), "messages")
+print("Creating table messages(%s)..." % table_id)
 
 client.use_database("message_board")
 client.use_table("messages")
 
+print("Waiting for primary lease...")
+
 client.set("index", "0")
+print("Initializing messages:index to 0...")
