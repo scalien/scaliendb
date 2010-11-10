92bb7b1433a571565e6dae51164ad12a8e6eb807
Attila Gazso <agazso@scalien.com>
2010-11-10 13:00:56 +0100

    Fixed stf.py.

--- test/stf.py
+++ test/stf.py
@@ -164,7 +164,7 @@ def test_run(file, funcs = None):
 	cc.add_cflag("-DTEST_FILE")
 	obj = cc.compile(input, output)
 	ld = Linker(LDPATH, LDLIBS)
-	objects = find_objects(BUILD_DIR, ["Main.o", "ScalienDB"], ["Test"])
+	objects = find_objects(BUILD_DIR, ["Main.o", "ScalienDB", "TestMain"], ["Test"])
 	objects.append(BUILD_DIR + "/" + TEST_DIR + "Test.o")
 	objects.append(obj)
 	output = BUILD_DIR + "/" + TEST_DIR + "TestProgram" #+ str(uuid.uuid1())
