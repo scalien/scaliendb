9829fbf94e4f2d56deee7a3635e6887affc942c8
Attila Gazso <agazso@scalien.com>
2010-11-12 10:06:00 +0100

    Fixed concurrent words test.

--- test/java/conword.sh
+++ test/java/conword.sh
@@ -2,7 +2,7 @@
 
 export LD_LIBRARY_PATH=.
 
-for i in {1..50}
+for i in {1..10}
 do
-        ./runforever ConcurrentWords 192.168.39.17:7800 &
+        ./runforever ConcurrentWords 192.168.39.17:7080 &
 done
