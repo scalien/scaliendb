adcff0e6f215bf44e67a1f14f3d9a6d776e91aee
Attila Gazso <agazso@scalien.com>
2010-12-30 19:40:03 +0100

    Added rps to random_batched_set.py output.

--- test/python/random_batched_set.py
+++ test/python/random_batched_set.py
@@ -1,6 +1,7 @@
 import random
 import sys
 import scaliendb
+import time
 
 CONTROLLERS=["localhost:7080"]
 #CONTROLLERS=["192.168.137.51:7080"]
@@ -29,10 +30,12 @@ value = "%100s" % " "
 i = 0
 batch = 10000
 while limit == 0 or sent < limit:
+	start = time.clock()
 	client.begin()
 	for x in xrange(batch):
 		client.set(str(random.randint(1, 1000000000)), value)
 		sent += len(value)
 	client.submit()
+	end = time.clock()
 	i += batch
-	print("Sent bytes: %s, num: %i" % (sizeof_fmt(sent), i))
+	print("Sent bytes: %s, num: %i, rps = %.0f" % (sizeof_fmt(sent), i, (batch/((end - start) * 1000.0) * 1000.0)))
