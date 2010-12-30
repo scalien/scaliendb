ee6a06f5b97e02bc1a72fc3601143140d6a4ef41
Attila Gazso <agazso@scalien.com>
2010-12-30 19:44:26 +0100

    Fixed StorageFileChunk destruction temporarily.

--- src/Framework/Storage/StorageEnvironment.cpp
+++ src/Framework/Storage/StorageEnvironment.cpp
@@ -150,6 +150,7 @@ void StorageEnvironment::Close()
 
     // TODO: clean up fileChunks properly, see the issue with StorageShard::chunkList
     //fileChunks.DeleteList();
+    fileChunks.Clear();
 }
 
 void StorageEnvironment::SetStorageConfig(StorageConfig& config_)
