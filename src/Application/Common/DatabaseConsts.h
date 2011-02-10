ab5199aeb0f43d2f9c7052ded899bf5204f2bfa9
Marton Trencseni <mtrencseni@gmail.com>
2011-02-10 17:06:36 +0100

    Added compression to shard replication.

--- src/Application/Common/DatabaseConsts.h
+++ src/Application/Common/DatabaseConsts.h
@@ -3,13 +3,10 @@
 
 #include "System/Common.h"
 
-#define DATABASE_NAME_SIZE          ( 1*KiB)
+#define DATABASE_NAME_SIZE                      ( 1*KiB)
+#define DATABASE_KEY_SIZE                       ( 1*KiB)
+#define DATABASE_VAL_SIZE                       (16*MiB)
 
-#define DATABASE_KEY_SIZE           ( 1*KiB)
-#define DATABASE_VAL_SIZE           (16*MiB)
-
-// TODO:
-//#define DATABASE_REPLICATION_SIZE   (1*KiB + DATABASE_KEY_SIZE + DATABASE_VAL_SIZE)
-#define DATABASE_REPLICATION_SIZE   (4000*KiB)
+#define DATABASE_UNCOMPRESSED_REPLICATION_SIZE  (128*KiB)
 
 #endif
