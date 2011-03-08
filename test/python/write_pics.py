6157b74b55205362d39714d49b57d8a832070bde
Marton Trencseni <mtrencseni@gmail.com>
2011-03-08 14:03:06 +0100

    Changed Python bulk test script.

--- test/python/write_pics.py
+++ test/python/write_pics.py
@@ -2,20 +2,32 @@ import scaliendb
 from scaliendb_load import *
 
 client = scaliendb.Client(["localhost:7080"])
+#client = scaliendb.Client(["192.168.137.51:7080"])
 #client._set_trace(True)
 client.use_database("bulk")
 client.use_table("pics")
 lr = scaliendb_loader(client, print_stats=True)
 f = open('data/pics.txt','r') 
-lr.begin()
-i = None
+images = {}
+dataset = 500*1000*1000
+size = 0
+print("Reading images from disk...")
 for key in f:
 	key = key.rstrip()
-	if i is None:
-		i = open(key, 'r')
-		value = i.read()
+	i = open(key, 'r')
+	value = i.read()
+	images[key] = value
+	size += len(value)
+	if size > dataset:
+		break
+print("Done.")
+lr.begin()
+for key, value in images.iteritems():
+# for key in f:
+# 	key = key.rstrip()
+# 	i = open(key, 'r')
+# 	value = i.read()
 	lr.set(key, value)
-	#print(key)
 lr.submit()
 print("Done.")
 f.close()
