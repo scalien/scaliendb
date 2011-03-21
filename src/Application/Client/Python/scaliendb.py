a5eba3fcfc2aced9280c6881d18e618a87956911
Attila Gazso <agazso@gmail.com>
2011-03-21 13:58:30 +0100

    Fixed python tab issue.

--- src/Application/Client/Python/scaliendb.py
+++ src/Application/Client/Python/scaliendb.py
@@ -324,36 +324,36 @@ class Client:
 class Loader:        
     def __init__(self, client, granularity = 1000*1000, print_stats = False):
         self.client = client
-		self.granularity = granularity
-		self.print_stats = print_stats
-		self.items_batch = 0
-		self.items_total = 0
-		self.bytes_batch = 0
-		self.bytes_total = 0
-		self.elapsed = 0
+        self.granularity = granularity
+        self.print_stats = print_stats
+        self.items_batch = 0
+        self.items_total = 0
+        self.bytes_batch = 0
+        self.bytes_total = 0
+        self.elapsed = 0
     
     def begin(self):
-		self.client.begin()
-		self.items_batch = 0
-		self.bytes_batch = 0
-	
-	def submit(self):
-		starttime = time.time()
-		self.client.submit()
-		endtime = time.time()
-		self.elapsed += (endtime - starttime)
-		if self.print_stats:
-			endtimestamp = time.strftime("%H:%M:%S", time.gmtime())
-			print("%s:\t total items: %s \t total bytes: %s \t aggregate thruput: %s/s" % (endtimestamp, self.items_total, sizeof_fmt(self.bytes_total), sizeof_fmt(self.bytes_total/((self.elapsed) * 1000.0) * 1000.0)))
+        self.client.begin()
+        self.items_batch = 0
+        self.bytes_batch = 0
+    
+    def submit(self):
+        starttime = time.time()
+        self.client.submit()
+        endtime = time.time()
+        self.elapsed += (endtime - starttime)
+        if self.print_stats:
+            endtimestamp = time.strftime("%H:%M:%S", time.gmtime())
+            print("%s:\t total items: %s \t total bytes: %s \t aggregate thruput: %s/s" % (endtimestamp, self.items_total, sizeof_fmt(self.bytes_total), sizeof_fmt(self.bytes_total/((self.elapsed) * 1000.0) * 1000.0)))
 
-	def set(self, key, value):
-		l = len(key) + len(value)
-		self.client.set(key, value)
-		self.items_batch += 1
-		self.items_total += 1
-		self.bytes_batch += l
-		self.bytes_total += l
-		if self.bytes_batch > self.granularity:
-			self.submit()
-			self.begin()
+    def set(self, key, value):
+        l = len(key) + len(value)
+        self.client.set(key, value)
+        self.items_batch += 1
+        self.items_total += 1
+        self.bytes_batch += l
+        self.bytes_total += l
+        if self.bytes_batch > self.granularity:
+            self.submit()
+            self.begin()
 
