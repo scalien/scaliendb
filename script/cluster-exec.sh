cc6c8a73fa65b6d0a71c36c1d30b9627f7bf7506
Attila Gazso <agazso@gmail.com>
2011-04-21 15:01:25 +0200

    Updated cluster-exec script.

--- script/cluster-exec.sh
+++ script/cluster-exec.sh
@@ -8,7 +8,7 @@ for shard_server in $SCALIENDB_SHARDSERVERS; do
         remote_cmd="cd scaliendb; $CMD"
         echo "$shard_server:$$ $remote_cmd"
         remote_cmd_redirect="$remote_cmd 2>&1 1>/dev/null"
-        ssh $shard_server $remote_cmd_redirect 2>&1 1>/dev/null | sed s/^/$shard_server:/ &
+        ssh $shard_server $remote_cmd_redirect 2>&1 1>/dev/null | sed s/^/$shard_server: / &
 done
 
 wait
