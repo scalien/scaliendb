a442c16e4dce14b18ed09ac970437b61b5352549
Attila Gazso <agazso@gmail.com>
2011-04-28 18:23:47 +0200

    Update Java SWIG interface.

--- src/Application/Client/Java/scaliendb_clientJNI.java
+++ src/Application/Client/Java/scaliendb_clientJNI.java
@@ -111,4 +111,6 @@ class scaliendb_clientJNI {
   public final static native int SDBP_Cancel(long jarg1);
   public final static native boolean SDBP_IsBatched(long jarg1);
   public final static native void SDBP_SetTrace(boolean jarg1);
+  public final static native String SDBP_GetVersion();
+  public final static native String SDBP_GetDebugString();
 }
