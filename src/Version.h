e06fd5d76fa0c23d7e1965ae5f644e388adec74b
Marton Trencseni <mtrencseni@gmail.com>
2011-05-10 16:06:45 +0200

    Upped version to 1.9.9

--- src/Version.h
+++ src/Version.h
@@ -1,7 +1,7 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define VERSION_MAJOR       "0"
+#define VERSION_MAJOR       "1"
 #define VERSION_MINOR       "9"
 #define VERSION_RELEASE     "9"
 #define VERSION_STRING      VERSION_MAJOR "." VERSION_MINOR "." VERSION_RELEASE
