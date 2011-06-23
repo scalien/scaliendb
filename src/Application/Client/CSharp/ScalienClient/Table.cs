923634bc1d3f1a020d104026e43dce5298a57be6
agazso <agazso@gmail.com>
2011-06-23 17:41:40 +0200

    Added Table functions to CSharp client.

--- src/Application/Client/CSharp/ScalienClient/Table.cs
+++ src/Application/Client/CSharp/ScalienClient/Table.cs
@@ -42,5 +42,83 @@ namespace Scalien
         {
             client.TruncateTable(tableID);
         }
+
+        public string Get(string key)
+        {
+            UseDefaults();
+            return client.Get(key);
+        }
+
+        public void Set(string key, string value)
+        {
+            UseDefaults();
+            client.Set(key, value);
+        }
+
+        public bool SetIfNotExists(string key, string value)
+        {
+            UseDefaults();
+            return client.SetIfNotExists(key, value);
+        }
+
+        public bool TestAndSet(string key, string test, string value)
+        {
+            UseDefaults();
+            return client.TestAndSet(key, test, value);
+        }
+
+        public string GetAndSet(string key, string value)
+        {
+            UseDefaults();
+            return client.GetAndSet(key, value);
+        }
+
+        public long Add(string key, long number)
+        {
+            UseDefaults();
+            return client.Add(key, number);
+        }
+
+        public void Append(string key, string value)
+        {
+            UseDefaults();
+            client.Append(key, value);
+        }
+
+        public void Delete(string key)
+        {
+            UseDefaults();
+            client.Delete(key);
+        }
+
+        public bool TestAndDelete(string key, string test)
+        {
+            UseDefaults();
+            return client.TestAndDelete(key, test);
+        }
+
+        public string Remove(string key)
+        {
+            UseDefaults();
+            return client.Remove(key);
+        }
+
+        public List<string> ListKeys(string startKey, string endKey, string prefix, uint offset, uint count)
+        {
+            UseDefaults();
+            return client.ListKeys(startKey, endKey, prefix, offset, count);
+        }
+
+        public Dictionary<string, string> ListKeyValues(string startKey, string endKey, string prefix, uint offset, uint count)
+        {
+            UseDefaults();
+            return client.ListKeyValues(startKey, endKey, prefix, offset, count);
+        }
+
+        public ulong Count(string startKey, string endKey, string prefix, uint offset, uint count)
+        {
+            UseDefaults();
+            return Count(startKey, endKey, prefix, offset, count);
+        }
     }
 }
