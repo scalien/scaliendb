b63ba3456896e68755117089933dfd387652b60f
agazso <agazso@scalien.com>
2011-08-23 19:42:05 +0200

    ScalienDB 2.0

--- src/Version.h
+++ src/Version.h
@@ -1,9 +1,9 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define VERSION_MAJOR       "1"
-#define VERSION_MINOR       "9"
-#define VERSION_RELEASE     "9"
+#define VERSION_MAJOR       "2"
+#define VERSION_MINOR       "0"
+#define VERSION_RELEASE     "0"
 #define VERSION_STRING      VERSION_MAJOR "." VERSION_MINOR "." VERSION_RELEASE
 
 #endif
