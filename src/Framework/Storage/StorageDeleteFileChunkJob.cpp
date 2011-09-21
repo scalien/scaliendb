a4a6501c66b3d8ce60ff5ddcdd57a641b28756a8
mtrencseni <mtrencseni@gmail.com>
2011-09-21 16:08:57 +0200

    Write chunks during log recovery.

--- src/Framework/Storage/StorageDeleteFileChunkJob.cpp
+++ src/Framework/Storage/StorageDeleteFileChunkJob.cpp
@@ -11,13 +11,14 @@ StorageDeleteFileChunkJob::StorageDeleteFileChunkJob(StorageFileChunk* chunk_)
 void StorageDeleteFileChunkJob::Execute()
 {
     Stopwatch   sw;
+    Buffer      filename;
     
     Log_Debug("Deleting file chunk %U from disk, starting", chunk->GetChunkID());
     sw.Start();
     
-    FS_Delete(chunk->GetFilename().GetBuffer());
-
+    filename.Write(chunk->GetFilename());
     delete chunk;
+    FS_Delete(filename.GetBuffer());
 
     sw.Stop();
     Log_Debug("Deleted, elapsed: %U", (uint64_t) sw.Elapsed());
