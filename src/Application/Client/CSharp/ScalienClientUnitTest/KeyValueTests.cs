02c0005e9c154b9065337bde14f7471628a27503
szabozs <szabozs@gmail.com>
2011-09-22 15:08:16 +0200

    ScalienClient unit test project added.

---
+++ src/Application/Client/CSharp/ScalienClientUnitTest/KeyValueTests.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Scalien;
+
+namespace ScalienClientUnitTesting
+{
+    //[TestClass]
+    class KeyValueTests
+    {
+        [TestMethod]
+        public void RandomKey_Values_CheckUsingGetByKey()
+        {
+            string dbName = "random_key_values_db";
+            string tableName = "random_key_values_tbl";
+
+            Client client = new Client(Config.controllers);
+            Utils.deleteDBs(client);
+
+            Database db = client.CreateDatabase(dbName);
+            Table tbl = db.CreateTable(tableName);
+            byte[][] key = new byte[50000][];
+            byte[][] value = new byte[50000][];
+            
+            for (int i = 0; i < 50000; i++)
+            {
+                key[i] = Utils.RandomASCII();
+                value[i] = Utils.RandomASCII();
+
+                tbl.Set(key[i], value[i]);
+            }
+
+            client.Submit();
+
+            for (int i = 0; i < 50000; i++)
+            {
+                byte[] val = tbl.Get(key[i]);
+                Assert.IsNotNull(val);
+                Assert.IsTrue(Utils.byteArraysEqual(value[i], val));
+            }
+        }
+
+        [TestMethod]
+        public void RandomKey_Values_CheckUsingGetByKey_FromKeyIteratorAll()
+        {
+            string dbName = "random_key_values_db";
+            string tableName = "random_key_values_tbl";
+
+            Client client = new Client(Config.controllers);
+            Utils.deleteDBs(client);
+
+            Database db = client.CreateDatabase(dbName);
+            Table tbl = db.CreateTable(tableName);
+            byte[][] key = new byte[10000][];
+            byte[][] value = new byte[10000][];
+            
+            for (int i = 0; i < 10000; i++)
+            {
+                key[i] = Utils.RandomASCII();
+                value[i] = Utils.RandomASCII();
+
+                tbl.Set(key[i], value[i]);
+            }
+
+            client.Submit();
+
+            int j = 0;
+            foreach (byte[] k in tbl.GetKeyIterator(new ByteRangeParams()))
+            {
+                byte[] val = tbl.Get(k);
+                Assert.IsNotNull(val);
+                Assert.IsTrue(Utils.byteArraysEqual(value[j++], val));
+            }
+        }
+
+        [TestMethod]
+        public void RandomKey_Values_CheckUsingGetByKey_FromKeyIteratorPrefix()
+        {
+            string dbName = "random_key_values_db";
+            string tableName = "random_key_values_tbl";
+
+            Client client = new Client(Config.controllers);
+            Utils.deleteDBs(client);
+
+            Database db = client.CreateDatabase(dbName);
+            Table tbl = db.CreateTable(tableName);
+            byte[][] key = new byte[10000][];
+            byte[][] value = new byte[10000][];
+            // byte array!!!
+            for (int i = 0; i < 10000; i++)
+            {
+                key[i] = Utils.RandomASCII();
+                value[i] = Utils.RandomASCII();
+
+                tbl.Set(key[i], value[i]);
+            }
+
+            client.Submit();
+
+            // todo
+            int j = 0;
+            foreach (byte[] k in tbl.GetKeyIterator(new ByteRangeParams()))
+            {
+                byte[] val = tbl.Get(k);
+                Assert.IsNotNull(val);
+                Assert.IsTrue(Utils.byteArraysEqual(value[j++], val));
+            }
+        }
+
+        [TestMethod]
+        public void RandomKey_Values_CheckUsingValueIteratorAll()
+        {
+            string dbName = "random_key_values_db";
+            string tableName = "random_key_values_tbl";
+
+            Client client = new Client(Config.controllers);
+            Utils.deleteDBs(client);
+
+            Database db = client.CreateDatabase(dbName);
+            Table tbl = db.CreateTable(tableName);
+            byte[][] key = new byte[10000][];
+            byte[][] value = new byte[10000][];
+            // byte array!!!
+            for (int i = 0; i < 10000; i++)
+            {
+                key[i] = Utils.RandomASCII();
+                value[i] = Utils.RandomASCII();
+
+                tbl.Set(key[i], value[i]);
+            }
+
+            client.Submit();
+            //TODO
+        }
+
+        [TestMethod]
+        public void RandomKey_Values_CheckUsingValueIteratorPrefix()
+        {
+            string dbName = "random_key_values_db";
+            string tableName = "random_key_values_tbl";
+
+            Client client = new Client(Config.controllers);
+            Utils.deleteDBs(client);
+
+            Database db = client.CreateDatabase(dbName);
+            Table tbl = db.CreateTable(tableName);
+            byte[][] key = new byte[10000][];
+            byte[][] value = new byte[10000][];
+            // byte array!!!
+            for (int i = 0; i < 10000; i++)
+            {
+                key[i] = Utils.RandomASCII();
+                value[i] = Utils.RandomASCII();
+
+                tbl.Set(key[i], value[i]);
+            }
+
+            client.Submit();
+            //TODO
+        }
+    }
+}
---
+++ src/Application/Client/CSharp/ScalienClientUnitTest/TestingFramework/Assert.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ScalienClientUnitTesting
+{
+    class UnitTestException : Exception
+    {
+        private string message;
+
+        public UnitTestException()
+        {
+            message = "";
+        }
+        public UnitTestException(string msg)
+        {
+            message = msg;
+        }
+        public string GetMessage()
+        {
+            return message;
+        }
+    }
+
+    class Assert
+    {
+        public static void Throw(string message)
+        {
+            throw new UnitTestException(message);
+        }
+        public static void IsTrue(bool cond)
+        {
+            if (cond != true) throw new UnitTestException("Assert.IsTrue failed");
+        }
+        public static void IsTrue(bool cond, string message)
+        {
+            if (cond != true) throw new UnitTestException(message);
+        }
+        public static void IsFalse(bool cond)
+        {
+            if (cond != false) throw new UnitTestException("Assert.IsFalse failed");
+        }
+        public static void IsFalse(bool cond, string message)
+        {
+            if (cond != false) throw new UnitTestException(message);
+        }
+        public static void IsNull(object value)
+        {
+            if (value != null) throw new UnitTestException("Assert.IsNull failed");
+        }
+        public static void IsNull(object value, string message)
+        {
+            if (value != null) throw new UnitTestException(message);
+        }
+        public static void IsNotNull(object value)
+        {
+            if (value == null) throw new UnitTestException("Assert.IsNotNull failed");
+        }
+        public static void IsNotNull(object value, string message)
+        {
+            if (value == null) throw new UnitTestException(message);
+        }
+
+    }
+}
