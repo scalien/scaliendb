9fb936d1605e2f163f3db67acf89a2ca69b975fc
agazso <agazso@scalien.com>
2011-10-06 23:58:26 +0200

    Added create source control version script.

---
+++ script/createscversion.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+COMMIT_ID=`git log -n1 --pretty=oneline | cut -f 1 -d ' '`
+OUTPUT=src/SourceControl.h
+
+echo > $OUTPUT
+echo "#ifndef SOURCE_CONTROL_H" >> $OUTPUT
+echo "#define SOURCE_CONTROL_H" >> $OUTPUT
+echo >> $OUTPUT
+echo "#define SOURCE_CONTROL_VERSION \"$COMMIT_ID\"" >> $OUTPUT
+echo >> $OUTPUT
+echo "#endif" >> $OUTPUT
+echo >> $OUTPUT
