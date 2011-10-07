26f4a9b68ef35d06b226e87d6481881d5897f779
agazso <agazso@scalien.com>
2011-10-07 14:50:01 +0200

    Added resource file to ScalienDB VC project.

--- script/replaceversion.cmd
+++ script/replaceversion.cmd
@@ -2,6 +2,9 @@
 SET FILENAME=%1
 FOR /F %%a IN ('..\script\version.cmd ..\src\Version.h . -extra') DO SET DOTTED_VERSION=%%a
 FOR /F %%a IN ('..\script\version.cmd ..\src\Version.h -comma -extra') DO SET COMMA_VERSION=%%a
+FOR /F %%a IN ('..\script\rcversion.cmd %FILENAME%') DO SET RCVERSION=%%a
+IF "%COMMA_VERSION%"=="%RCVERSION%" GOTO END
 PowerShell -ExecutionPolicy RemoteSigned -File ..\script\replaceversion.ps1 %FILENAME% %COMMA_VERSION% %DOTTED_VERSION% > %FILENAME%.temp
 DEL %FILENAME%
 REN %FILENAME%.temp %FILENAME%
+:END
