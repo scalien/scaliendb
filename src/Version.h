8af3779c884bf96e955ae04df9c03cdf6e5c74c4
agazso <agazso@scalien.com>
2011-10-07 14:50:34 +0200

    Bumped version number to 2.0.8

--- src/Version.h
+++ src/Version.h
@@ -3,7 +3,7 @@
 
 #define VERSION_MAJOR       "2"
 #define VERSION_MINOR       "0"
-#define VERSION_RELEASE     "7"
+#define VERSION_RELEASE     "8"
 #define VERSION_STRING      VERSION_MAJOR "." VERSION_MINOR "." VERSION_RELEASE
 
 #endif
