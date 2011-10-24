2d522e193ca53b18985244548aec181696aac97b
agazso <agazso@scalien.com>
2011-10-24 18:52:09 +0200

    Bumped version number to 2.0.11

--- src/Version.h
+++ src/Version.h
@@ -3,7 +3,7 @@
 
 #define VERSION_MAJOR       "2"
 #define VERSION_MINOR       "0"
-#define VERSION_RELEASE     "10"
+#define VERSION_RELEASE     "11"
 #define VERSION_STRING      VERSION_MAJOR "." VERSION_MINOR "." VERSION_RELEASE
 
 #endif
