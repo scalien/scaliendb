7aca8a48f58a0e52cfe8e419db9360c9825e88b8
agazso <agazso@scalien.com>
2012-01-12 19:04:03 +0100

    Added error handling to release script.

--- script/copyrelease.cmd
+++ script/copyrelease.cmd
@@ -9,10 +9,25 @@ ECHO Creating build directory: %BUILD_DIR%
 
 RMDIR /S /Q %BUILD_DIR%
 MD %BUILD_DIR%
+IF %ERRORLEVEL% NEQ 0 EXIT /B 1
+
 COPY %BIN_DIR%\ScalienDB.exe %BUILD_DIR%
+IF %ERRORLEVEL% NEQ 0 EXIT /B 1
+
 COPY %BIN_DIR%\scaliendb_client.dll %BUILD_DIR%
+IF %ERRORLEVEL% NEQ 0 EXIT /B 1
+
 COPY %BIN_DIR%\ScalienClient.dll %BUILD_DIR%
+IF %ERRORLEVEL% NEQ 0 EXIT /B 1
+
 COPY ..\test\control\release\controller.conf %BUILD_DIR%\controller.conf
+IF %ERRORLEVEL% NEQ 0 EXIT /B 1
+
 COPY ..\test\shard\release\shard.conf %BUILD_DIR%\shard.conf
+IF %ERRORLEVEL% NEQ 0 EXIT /B 1
+
 XCOPY /q /e /i ..\webadmin %BUILD_DIR%\webadmin
+IF %ERRORLEVEL% NEQ 0 EXIT /B 1
+
 CALL ..\script\ziprelease.cmd %BUILD_ARCH_DIR% %BUILD_NAME%
+IF %ERRORLEVEL% NEQ 0 EXIT /B 1
--- script/release.cmd
+++ script/release.cmd
@@ -1,6 +1,9 @@
 @ECHO OFF
 
+SET BASEDIR=%CD%
 SET OUTPUT=release.log
+: Possible values are: quiet, minimal, normal, detailed, diagnostic
+SET VERBOSITY=minimal
 
 ECHO.
 ECHO ============================================
@@ -11,7 +14,9 @@ ECHO ============================================
 ECHO.
 CD ScalienDB.vcproj
 CALL ..\script\buildrelease.cmd x64
+IF %ERRORLEVEL% NEQ 0 GOTO ERROR
 CALL ..\script\buildrelease.cmd Win32
+IF %ERRORLEVEL% NEQ 0 GOTO ERROR
 CHDIR /D %BASEDIR%
 
 ECHO.
@@ -22,7 +27,8 @@ ECHO.
 ECHO ============================================
 ECHO.
 CD src\Application\Client\CSharp\ScalienClientWithNativeDLL
-CALL msbuild ScalienClientWithNativeDLL.csproj /v:minimal /t:Rebuild /p:Configuration=Debug
+CALL msbuild ScalienClientWithNativeDLL.csproj /v:%VERBOSITY% /t:Rebuild /p:Configuration=Debug
+IF %ERRORLEVEL% NEQ 0 GOTO ERROR
 CHDIR /D %BASEDIR%
 
 ECHO.
@@ -34,7 +40,9 @@ ECHO ============================================
 ECHO.
 CD ScalienDB.vcproj
 CALL ..\script\copyrelease.cmd x64
+IF %ERRORLEVEL% NEQ 0 GOTO ERROR
 CALL ..\script\copyrelease.cmd Win32
+IF %ERRORLEVEL% NEQ 0 GOTO ERROR
 CHDIR /D %BASEDIR%
 
 ECHO.
@@ -44,4 +52,15 @@ ECHO Release is done.
 ECHO.
 ECHO ============================================
 ECHO.
+EXIT /B 0
 
+:ERROR
+ECHO.
+ECHO ============================================
+ECHO.
+ECHO An error occured!
+ECHO.
+ECHO ============================================
+ECHO.
+CHDIR /D %BASEDIR%
+EXIT /B 1
