cb7e364bc6ac982bcfbf47f74a8aad363d32b8dc
agazso <agazso@scalien.com>
2012-01-20 16:05:03 +0100

    Bumped version number.

--- src/Version.h
+++ src/Version.h
@@ -3,7 +3,8 @@
 
 #define VERSION_MAJOR       "2"
 #define VERSION_MINOR       "2"
-#define VERSION_RELEASE     "4"
+#define VERSION_RELEASE     "5"
+// keep VERSION_PATCH empty for release versions
 #define VERSION_PATCH       "-rc1"
 #define VERSION_STRING      VERSION_MAJOR "." VERSION_MINOR "." VERSION_RELEASE VERSION_PATCH
 
