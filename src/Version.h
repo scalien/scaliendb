0a0fc40acb37d2c8c068696c6ab9674cf051f003
agazso <agazso@scalien.com>
2012-01-24 15:46:18 +0100

    Bumped version number to 2.2.5

--- src/Version.h
+++ src/Version.h
@@ -5,7 +5,7 @@
 #define VERSION_MINOR       "2"
 #define VERSION_RELEASE     "5"
 // keep VERSION_PATCH empty for release versions
-#define VERSION_PATCH       "-rc2"
+#define VERSION_PATCH       ""
 #define VERSION_STRING      VERSION_MAJOR "." VERSION_MINOR "." VERSION_RELEASE VERSION_PATCH
 
 #endif
