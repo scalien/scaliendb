225a3295cf6d20d71fec6d56488c45d0e6759529
agazso <agazso@scalien.com>
2012-02-01 17:22:48 +0100

    Treat warnings as errors in release builds.

--- src/Application/Client/CSharp/ScalienClientWithNativeDLL/Properties/AssemblyInfo.cs
+++ src/Application/Client/CSharp/ScalienClientWithNativeDLL/Properties/AssemblyInfo.cs
@@ -33,7 +33,8 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("2.2.5.0")]
+[assembly: AssemblyFileVersion("2.2.6.0")]
+
 
 
 
