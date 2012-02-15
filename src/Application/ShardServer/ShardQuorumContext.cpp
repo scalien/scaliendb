eb31ca9f0281394854b5242092fa4d85d7245f51
mtrencseni <mtrencseni@gmail.com>
2012-02-15 16:16:31 +0100

    Fixed bug where dropped Paxos message gets the paxos queue stuck.

--- src/Application/ShardServer/ShardQuorumContext.cpp
+++ src/Application/ShardServer/ShardQuorumContext.cpp
@@ -325,6 +325,7 @@ void ShardQuorumContext::OnPaxosMessage(ReadBuffer buffer)
         if (!quorum.IsMember(msg.nodeID))
         {
             Log_Debug("Dropping paxos msg from %U because that node is not a quourm member", msg.nodeID);
+            OnMessageProcessed();
             return;
         }
     }
