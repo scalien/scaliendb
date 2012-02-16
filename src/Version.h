9bbf6923a9c751a8c68951ea7664fc398ff9e6e3
agazso <agazso@scalien.com>
2012-02-16 11:04:44 +0100

    Added ScalienClientJavaDLL project to VS solution.

--- src/Version.h
+++ src/Version.h
@@ -5,7 +5,7 @@
 #define VERSION_MINOR       "2"
 #define VERSION_RELEASE     "6"
 // keep VERSION_PATCH empty for release versions
-#define VERSION_PATCH       "-rc15"
+#define VERSION_PATCH       "-rc17"
 #define VERSION_STRING      VERSION_MAJOR "." VERSION_MINOR "." VERSION_RELEASE VERSION_PATCH
 
 #endif
