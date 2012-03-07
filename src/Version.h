2c5bd9b70fea7395499fa17fa08afb6863bf5935
agazso <agazso@scalien.com>
2012-03-07 16:37:34 +0100

    Bumped version number to 2.2.7

--- src/Version.h
+++ src/Version.h
@@ -3,9 +3,9 @@
 
 #define VERSION_MAJOR       "2"
 #define VERSION_MINOR       "2"
-#define VERSION_RELEASE     "6"
+#define VERSION_RELEASE     "7"
 // keep VERSION_PATCH empty for release versions
-#define VERSION_PATCH       "-rc30"
+#define VERSION_PATCH       ""
 #define VERSION_STRING      VERSION_MAJOR "." VERSION_MINOR "." VERSION_RELEASE VERSION_PATCH
 
 #endif
