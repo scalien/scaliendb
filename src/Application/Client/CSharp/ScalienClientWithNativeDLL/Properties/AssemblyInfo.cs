48375f570c18452b5edb0c9bcc4c4ff18ba582d7
agazso <agazso@scalien.com>
2012-03-26 15:05:05 +0200

    Bumped version number to 2.3.0.

--- src/Application/Client/CSharp/ScalienClientWithNativeDLL/Properties/AssemblyInfo.cs
+++ src/Application/Client/CSharp/ScalienClientWithNativeDLL/Properties/AssemblyInfo.cs
@@ -33,7 +33,8 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("2.2.13.0")]
+[assembly: AssemblyFileVersion("2.3.0.0")]
+
 
 
 
