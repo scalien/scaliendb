8d45867ca0dd913842a8e419f8f45043349a7012
agazso <agazso@scalien.com>
2012-05-25 15:58:32 +0200

    Bumped version number to 2.4.5

--- src/Version.h
+++ src/Version.h
@@ -3,9 +3,9 @@
 
 #define VERSION_MAJOR       "2"
 #define VERSION_MINOR       "4"
-#define VERSION_RELEASE     "4"
+#define VERSION_RELEASE     "5"
 // keep VERSION_PATCH empty for release versions
-#define VERSION_PATCH       "-patch26"
+#define VERSION_PATCH       ""
 #define VERSION_STRING      VERSION_MAJOR "." VERSION_MINOR "." VERSION_RELEASE VERSION_PATCH
 
 #endif
