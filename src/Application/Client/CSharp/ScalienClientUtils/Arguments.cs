bf18b768cfa1df3cc97ad4a82d9f66adea7d3e2a
agazso <agazso@scalien.com>
2012-06-21 11:40:14 +0200

    Added Arguments to ScalienClientUtils.

---
+++ src/Application/Client/CSharp/ScalienClientUtils/Arguments.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections.Specialized;
+using System.Text.RegularExpressions;
+
+namespace ScalienClientUtils
+{
+    /// <summary>
+    /// Arguments class
+    /// </summary>
+    public class Arguments
+    {
+        // Variables
+        private StringDictionary parameters;
+
+        // Constructor
+        public Arguments(string[] Args)
+        {
+            parameters = new StringDictionary();
+            Regex splitter = new Regex(@"^-{1,2}|^/|=", RegexOptions.IgnoreCase | RegexOptions.Compiled);
+            Regex Remover = new Regex(@"^['""]?(.*?)['""]?$", RegexOptions.IgnoreCase | RegexOptions.Compiled);
+            string parameter = null;
+            string[] parts;
+
+            // Valid parameters forms:
+            // {-,/,--}param{ ,=}((",')value(",'))
+            // Examples: -param1 value1 --param2 /param3="Test-:-work" /param4=happy -param5 '--=nice=--'
+            foreach (string Txt in Args)
+            {
+                // Look for new parameters (-,/ or --) and a possible enclosed value (=,:)
+                parts = splitter.Split(Txt, 3);
+                switch (parts.Length)
+                {
+                    // Found a value (for the last parameter found (space separator))
+                    case 1:
+                        if (parameter != null)
+                        {
+                            if (!parameters.ContainsKey(parameter))
+                            {
+                                parts[0] = Remover.Replace(parts[0], "$1");
+                                parameters.Add(parameter, parts[0]);
+                            }
+                            parameter = null;
+                        }
+                        // else Error: no parameter waiting for a value (skipped)
+                        break;
+                    // Found just a parameter
+                    case 2:
+                        // The last parameter is still waiting. With no value, set it to true.
+                        if (parameter != null)
+                        {
+                            if (!parameters.ContainsKey(parameter)) parameters.Add(parameter, "");
+                        }
+                        parameter = parts[1];
+                        break;
+                    // Parameter with enclosed value
+                    case 3:
+                        // The last parameter is still waiting. With no value, set it to true.
+                        if (parameter != null)
+                        {
+                            if (!parameters.ContainsKey(parameter)) parameters.Add(parameter, "");
+                        }
+                        parameter = parts[1];
+                        // Remove possible enclosing characters (",')
+                        if (!parameters.ContainsKey(parameter))
+                        {
+                            parts[2] = Remover.Replace(parts[2], "$1");
+                            parameters.Add(parameter, parts[2]);
+                        }
+                        parameter = null;
+                        break;
+                }
+            }
+            // In case a parameter is still waiting
+            if (parameter != null)
+            {
+                if (!parameters.ContainsKey(parameter)) parameters.Add(parameter, "");
+            }
+        }
+
+        // Retrieve a parameter value if it exists
+        public string this[string Param]
+        {
+            get
+            {
+                return (parameters[Param]);
+            }
+        }
+    }
+}
