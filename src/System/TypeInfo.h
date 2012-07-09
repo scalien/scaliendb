2c605f92aa35d427e1014f859455424bfa9595bb
agazso <agazso@scalien.com>
2012-07-09 17:55:05 +0200

    Changed TypeInfo to not be empty class in release version.

--- src/System/TypeInfo.h
+++ src/System/TypeInfo.h
@@ -9,35 +9,21 @@
 ===============================================================================================
 */
 
-#ifdef DEBUG
-#define TYPEINFO
-#endif // DEBUG
-
 class TypeInfo
 {
 public:
     TypeInfo(const char* type_)
     {
-#ifdef TYPEINFO
         type = type_;
-#else
-        (void) type_;
-#endif
     }
 
     const char* GetType()
     {
-#ifdef TYPEINFO
         return type;
-#else
-        return "";
-#endif
     }
 
-#ifdef TYPEINFO
 protected:
     const char* type;
-#endif
 };
 
 #endif
