c977ad036ff1749d44c3191430b298a5f69ae453
mtrencseni <mtrencseni@gmail.com>
2012-07-11 16:58:01 +0200

    Added header text.

--- src/System/Memory/Memory.h
+++ src/System/Memory/Memory.h
@@ -1,6 +1,15 @@
 #ifndef MEMORY_H
 #define MEMORY_H
 
+/*
+
+  nedmalloc is a VERY fast, VERY scalable, multithreaded
+  memory allocator with little memory fragmentation.
+
+  See http://www.nedprod.com/programs/portable/nedmalloc/
+
+*/
+
 #include "System/Memory/nedmalloc.h"
 
 // route C style memory management
