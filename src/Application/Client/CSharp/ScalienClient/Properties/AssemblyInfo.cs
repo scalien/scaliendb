24ba1c74bd0f193c189fef8849663334b88af12c
mtrencseni <mtrencseni@gmail.com>
2012-07-18 14:24:47 +0200

    AssemblyInfo 2.6.0

--- src/Application/Client/CSharp/ScalienClient/Properties/AssemblyInfo.cs
+++ src/Application/Client/CSharp/ScalienClient/Properties/AssemblyInfo.cs
@@ -30,7 +30,7 @@ using System.Runtime.InteropServices;
 //      Revision
 //
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("2.5.6.0")]
+[assembly: AssemblyFileVersion("2.6.0.0")]
 
 
 
